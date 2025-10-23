2778b1d55a76dd94bb8374e8f3e23a664b878070
Lucas Neo <lucasneo@outlook.com.br>
2025-10-22 22:03:41 -0300

    feat: [TDD red] add empty book module structure - 13/14 tests failing

---
+++ livro.py
@@ -0,0 +1,25 @@
+class Livro:
+    
+    def __init__(self, titulo, autor, isbn):
+        pass
+    
+    def emprestar(self):
+        pass
+    
+    def devolver(self):
+        pass
+
+def criar_livro(titulo, autor, isbn):
+    pass
+
+
+def listar_livros():
+    pass
+
+def buscar_livro_por_isbn(isbn):
+    pass
+
+def atualizar_livro(isbn, novo_titulo, novo_autor):
+    pass
+def deletar_livro(isbn):
+    pass
